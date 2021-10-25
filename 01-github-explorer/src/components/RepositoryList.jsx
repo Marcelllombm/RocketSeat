76d4c1d30c3c5facd1d6a5d2d33f318421e38c15
MarceloBorges <melombm@hotmail.com>
2021-10-24 22:08:14 -0500

    Primeiro componente react

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,43 @@
+const repositoryname = 'Uniform2';
+
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de Repositórios</h1>
+      <u>
+        <li>
+          <strong>{repositoryname}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </u>
+    </section>
+  );
+};
